46f8a4ff2fdf36d0ca6cb650090c4fab20ce52d4
Singriver <jortizv2015@gmail.com>
2023-05-26 23:13:56 +0200

    Change theme, add clock, handle edit and complete error

--- Day18/Program1.py
+++ Day18/Program1.py
@@ -1,10 +1,13 @@
 import PySimpleGUI as sp
 import functions
+import time
 
+sp.theme("DarkPurple5")
 
+clock = sp.Text("", key="clock")
 label = sp.Text("Type in a to do")
 input_box = sp.InputText(tooltip="Enter todo", key='todo')
-add_button = sp.Button("Add")
+add_button = sp.Button("Add", size=10)
 list_box = sp.Listbox(values=functions.get_todos(), key='todos_list',
                       enable_events=True, size=[45, 10])
 edit_button = sp.Button("Edit")
@@ -20,20 +23,21 @@ for bl in button_labels:
     layout.append([sp.Button(bl)])
 '''
 
-layout = [[label],
+layout = [[clock],[label],
           [input_box, add_button],
           [list_box, edit_button, complete_button],
           [exit_button]]
-# Do not add only one list; at least add two elements list type inside layout to avoid errors
+# Do not add only one list; at  least add two elements list type inside layout to avoid errors
 window = sp.Window('My To Do App',
                    layout=layout,
                    font=('Helvetica', 20))
 
 while True:
-    event, values = window.read()
-    print(1, event)
-    print(2, values)
-    print(3, values['todos_list'])
+    event, values = window.read(timeout=10)
+    window["clock"].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
+    # print(1, event)
+    # print(2, values)
+    # print(3, values['todos_list'])
     match event:
         case "Add":
             todos = functions.get_todos()
@@ -42,21 +46,26 @@ while True:
             functions.write_todos(todos)
             window['todos_list'].update(values=todos)
         case "Edit":
-            todo_to_edit = values['todos_list'][0]
-            new_todo = values['todo']
-
-            todos = functions.get_todos()
-            index = todos.index(todo_to_edit)
-            todos[index] = new_todo
-            functions.write_todos(todos)
-            window['todos_list'].update(values=todos)
+            try:
+                todo_to_edit = values['todos_list'][0]
+                new_todo = values['todo']
+                todos = functions.get_todos()
+                index = todos.index(todo_to_edit)
+                todos[index] = new_todo
+                functions.write_todos(todos)
+                window['todos_list'].update(values=todos)
+            except IndexError:
+                sp.popup("Please select an item first", font=("Helvetica",20) )
         case "Complete":
-            todo_to_complete = values['todos_list'][0]
-            todos = functions.get_todos()
-            todos.remove(todo_to_complete)
-            functions.write_todos(todos)
-            window['todos_list'].update(values=todos)
-            window['todo'].update(value="")
+            try:
+                todo_to_complete = values['todos_list'][0]
+                todos = functions.get_todos()
+                todos.remove(todo_to_complete)
+                functions.write_todos(todos)
+                window['todos_list'].update(values=todos)
+                window['todo'].update(value="")
+            except IndexError:
+                sp.popup("Please select an item first", font=("Helvetica",20) )
         case "Exit":
             break
         case 'todos_list':
