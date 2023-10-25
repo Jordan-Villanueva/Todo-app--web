d92fa1a8511af108daee5a1279bf3cd030e9df9e
Singriver <jortizv2015@gmail.com>
2023-10-25 15:37:25 +0200

    Add descriptoin on the new columns

--- Day23/main.py
+++ Day23/main.py
@@ -1,13 +1,21 @@
 import streamlit as st
 
+st.set_page_config(layout='wide')
+
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo.png")
+    st.image("images/photo.png", width=300)
 
 with col2:
     st.title("Ardit Sulce")
     content = """
     grwwfdsfsdf
     """
-    st.write(content)
+    st.info(content)
+
+content2 = """
+    grwwfdsfsdfsdgsdvsdgsgdsgsdgasgfgdsggfdgdfdhhfhdfsdfgsgdsgsdagsdg
+    """
+st.write(content2)
+# can use st.info(content)
