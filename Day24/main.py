53081db2272e2050b41c2c8df654703271715686
Singriver <jortizv2015@gmail.com>
2023-11-08 21:27:53 +0100

    Update to Day24

--- Day24/main.py
+++ Day24/main.py
@@ -3,10 +3,10 @@ import pandas
 
 st.set_page_config(layout='wide')
 
-col1, col2 = st.columns(2)
+col1, col2, col3 = st.columns(3)
 
 with col1:
-    st.image("images/photo.png", width=300)
+    st.image("images/1.png", width=300)
 
 with col2:
     st.title("Ardit Sulce")
@@ -20,19 +20,21 @@ content2 = """
     """
 st.write(content2)
 # can use st.info(content)
-col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
+col3, empty_col, col4, empty_col, col5 = st.columns([1.5, 0.5, 1.5, 0.5, 1.5])
 
-df = pandas.read_csv("data.csv", sep=";")
+df = pandas.read_csv("data.csv", sep=",")
 with col3:
-    for index, row in df[:10].iterrows():
-        st.header(row["title"])
-        st.write(row["description"])
+    for index, row in df[:4].iterrows():
+        st.header(row["first name"])
+        st.write(row["role"])
         st.image("images/" + row["image"])
-        st.write(f"[Source Code]({row['url']})")
 with col4:
-    for index, row in df[10:].iterrows():
-        st.header(row["title"])
-        st.write(row["description"])
+    for index, row in df[4:8].iterrows():
+        st.header(row["first name"])
+        st.write(row["role"])
         st.image("images/" + row["image"])
-        st.write(f"[Source Code]({row['url']})")
-
+with col5:
+    for index, row in df[8:].iterrows():
+        st.header(row["first name"])
+        st.write(row["role"])
+        st.image("images/" + row["image"])
