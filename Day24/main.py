c5b93bb0887f80363fe6c074b17ccee233e97fed
Singriver <jortizv2015@gmail.com>
2023-11-10 01:48:13 +0100

    Update to Day24

--- Day24/main.py
+++ Day24/main.py
@@ -22,19 +22,19 @@ st.write(content2)
 # can use st.info(content)
 col3, empty_col, col4, empty_col, col5 = st.columns([1.5, 0.5, 1.5, 0.5, 1.5])
 
-df = pandas.read_csv("data.csv", sep=",")
+df = pandas.read_csv("data.csv")
 with col3:
     for index, row in df[:4].iterrows():
-        st.header(row["first name"])
+        st.header(row["first name"]+" "+row["last name"])
         st.write(row["role"])
         st.image("images/" + row["image"])
 with col4:
     for index, row in df[4:8].iterrows():
-        st.header(row["first name"])
+        st.header(row["first name"]+" "+row["last name"])
         st.write(row["role"])
         st.image("images/" + row["image"])
 with col5:
     for index, row in df[8:].iterrows():
-        st.header(row["first name"])
+        st.header(row["first name"]+" "+row["last name"])
         st.write(row["role"])
         st.image("images/" + row["image"])
